9631bad29dd313cbd557e93be414a852d564d7fd
rickym-h <richardhull11@gmail.com>
2022-06-20 17:58:23 +0100

    added function to get adjacent nodes of a given node by id

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ for (let row = 0; row < 11; row++) {
         let myNode = document.createElement("div");
         myNode.classList.toggle("empty-cell");
         myNode.id = row.toString() + col.toString();
+        myNode.innerText = myNode.id;
         myRowOfDivs.appendChild(myNode);
     }
     if (row % 2 === 0) {
@@ -20,3 +21,41 @@ for (let row = 0; row < 11; row++) {
 
 
 
+function getAdjacentNodes(id) {
+    if (typeof id === "number") {
+        id = id.toString()
+    }
+    let row = Number(id[0]);
+    let col = Number(id[1]);
+
+    let myNodeList = [];
+
+    if (row % 2 === 0){
+        myNodeList.push(document.getElementById((row-1).toString() + (col-1).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + (col).toString()));
+        myNodeList.push(document.getElementById((row).toString() + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + (col).toString()));
+
+    } else {
+        myNodeList.push(document.getElementById((row-1).toString() + (col).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + (col).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + (col+1).toString()));
+    }
+
+    myNodeList.filter(x => !!x);
+
+    for (let i = 0; i < myNodeList.length; i++ ) {
+
+        console.log(myNodeList[i]);
+        myNodeList[i].classList.toggle("wall-cell");
+    }
+    return myNodeList;
+}
+
+
+getAdjacentNodes(26)
