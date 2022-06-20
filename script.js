358c62c57dd257cbabcaf2b9bd1b5aba0c045687
rickym-h <richardhull11@gmail.com>
2022-06-20 18:50:40 +0100

    added functionality for capybara to move
    added win/lose function

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 const container = document.querySelector(".game-container");
 
-for (let row = 0; row < 11; row++) {
-    let myRowOfDivs = document.createElement("div");
-    myRowOfDivs.classList.toggle("div-row")
-    for (let col = 0; col < 11; col++) {
-        let myNode = document.createElement("div");
-        myNode.classList.toggle("empty-cell");
-        myNode.id = row.toString() + col.toString();
-        //myNode.innerText = myNode.id;
 
-        myNode.addEventListener("click",function(e) {
-            console.log(e.target);
-            if ((!e.target.classList.contains("capybara") && (!e.target.hasChildNodes()))) {
-                e.target.classList.toggle("wall-cell")
-            }
-            //todo move capybara
-        })
+function initialiseGame() {
+    for (let row = 0; row < 11; row++) {
+        let myRowOfDivs = document.createElement("div");
+        myRowOfDivs.classList.toggle("div-row")
+        for (let col = 0; col < 11; col++) {
+            let myNode = document.createElement("div");
+            myNode.classList.toggle("empty-cell");
+            myNode.id = row.toString() + "-" + col.toString();
+            //myNode.innerText = myNode.id;
 
-        myRowOfDivs.appendChild(myNode);
+            myNode.addEventListener("click",function(e) {
+                if ((!e.target.classList.contains("capybara") && (!e.target.hasChildNodes()))) {
+                    e.target.classList.toggle("wall-cell")
+                }
+                //todo move capybara
+                let x = document.querySelector(".capybara").parentNode;
+                let split = x.id.split("-")
+                let potentialNodes = getAdjacentNodes(split[0], split[1])
+                console.log(potentialNodes)
+                if (potentialNodes.length === 0){
+                    alert("YOU WIN!!")
+                    return;
+                }
+
+                const randomElement = potentialNodes[Math.floor(Math.random() * potentialNodes.length)];
+                let newSplit = randomElement.id.split("-");
+                putCapybaraInCell(newSplit[0], newSplit[1]);
+
+
+
+            })
+
+            myRowOfDivs.appendChild(myNode);
+        }
+        if (row % 2 === 0) {
+            myRowOfDivs.childNodes[0].classList.toggle("null-cell")
+        }
+        else {
+            myRowOfDivs.childNodes[myRowOfDivs.childNodes.length-1].classList.toggle("null-cell")
+        }
+        container.appendChild(myRowOfDivs);
     }
-    if (row % 2 === 0) {
-        myRowOfDivs.childNodes[0].classList.toggle("null-cell")
-    }
-    else {
-        myRowOfDivs.childNodes[myRowOfDivs.childNodes.length-1].classList.toggle("null-cell")
-    }
-    container.appendChild(myRowOfDivs);
+    putCapybaraInCell(5,5)
 }
 
 
@@ -35,35 +53,39 @@ function isNodeValid(x) {
 
     }
     if (x.classList.contains("null-cell")) {
+        //todo lose game
+        alert("YOU LOSE")
+        location.reload();
         return false;
     }
     if (x.classList.contains("wall-cell")) {
         return false;
     }
 
-
     return true;
 }
 
 function getAdjacentNodes(row,col) {
+    row = Number(row);
+    col = Number(col);
 
     let myNodeList = [];
 
     if (row % 2 === 0){
-        myNodeList.push(document.getElementById((row-1).toString() + (col-1).toString()));
-        myNodeList.push(document.getElementById((row-1).toString() + (col).toString()));
-        myNodeList.push(document.getElementById((row).toString() + (col+1).toString()));
-        myNodeList.push(document.getElementById((row).toString() + (col-1).toString()));
-        myNodeList.push(document.getElementById((row+1).toString() + (col-1).toString()));
-        myNodeList.push(document.getElementById((row+1).toString() + (col).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + "-" + (col-1).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + "-" + (col).toString()));
+        myNodeList.push(document.getElementById((row).toString() + "-" + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + "-" + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + "-" + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + "-" + (col).toString()));
 
     } else {
-        myNodeList.push(document.getElementById((row-1).toString() + (col).toString()));
-        myNodeList.push(document.getElementById((row-1).toString() + (col+1).toString()));
-        myNodeList.push(document.getElementById((row).toString() + (col+1).toString()));
-        myNodeList.push(document.getElementById((row).toString() + (col-1).toString()));
-        myNodeList.push(document.getElementById((row+1).toString() + (col).toString()));
-        myNodeList.push(document.getElementById((row+1).toString() + (col+1).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + "-" + (col).toString()));
+        myNodeList.push(document.getElementById((row-1).toString() + "-" + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + "-" + (col+1).toString()));
+        myNodeList.push(document.getElementById((row).toString() + "-" + (col-1).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + "-" + (col).toString()));
+        myNodeList.push(document.getElementById((row+1).toString() + "-" + (col+1).toString()));
     }
 
     // Filter out null and wall cells
@@ -78,16 +100,11 @@ function putCapybaraInCell(row,col) {
         previousCell.remove();
     }
 
-    const newCell = document.getElementById(row.toString() + col.toString());
+    const newCell = document.getElementById(row.toString() + "-" + col.toString());
     let myCapybara = document.createElement("div");
     myCapybara.classList.toggle("capybara");
     newCell.appendChild(myCapybara);
 
 }
 
-getAdjacentNodes(6,1)
-
-putCapybaraInCell(3,5)
-putCapybaraInCell(1,6)
-putCapybaraInCell(2,4)
-putCapybaraInCell(4,2)
+initialiseGame()
